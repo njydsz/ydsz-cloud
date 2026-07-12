57d93a2b766a21d4f196887717d7f319cb4046b6
Marvin Lee <limw1888@126.com>
2026-07-13 00:13:08 +0800

    docs(notify): 添加各模块通知集成的包说明文件
    - 新增定时任务模块通知集成的package-info.java
    - 新增财务模块通知集成的package-info.java
    - 新增项目模块通知集成的package-info.java
    - 新增销售模块通知集成的package-info.java
    - 说明基于common-notify的AsyncNotifyService实现各模块通知功能
    - 标明作者信息及版本号1.2.0

---
+++ ydsz-pmis-backend/ydsz-pmis-finance/ydsz-pmis-finance-server/src/main/java/com/njydsz/pmis/finance/server/notify/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * 财务模块通知集成
+ *
+ * <p>基于 common-notify 的 AsyncNotifyService 实现财务相关通知的多渠道异步发送。
+ *
+ * @author ydsz-pmis-team
+ * @since 1.2.0
+ */
+package com.njydsz.pmis.finance.server.notify;
---
+++ ydsz-pmis-backend/ydsz-pmis-project/ydsz-pmis-project-server/src/main/java/com/njydsz/pmis/project/server/notify/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * 项目模块通知集成
+ *
+ * <p>基于 common-notify 的 AsyncNotifyService 实现项目告警通知的多渠道异步发送。
+ *
+ * @author ydsz-pmis-team
+ * @since 1.2.0
+ */
+package com.njydsz.pmis.project.server.notify;
