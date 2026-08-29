6cad5e566e74927ed9b67b6da8c57a49f8551b58
Marvin Lee <limw1888@126.com>
2026-08-29 15:55:42 +0800

    fix: 修复 ClusterMigrationController 集群迁移逻辑

--- ydsz-cronjob/ydsz-cronjob-web/src/main/java/com/njydsz/cronjob/web/controller/cluster/ClusterMigrationController.java
+++ ydsz-cronjob/ydsz-cronjob-web/src/main/java/com/njydsz/cronjob/web/controller/cluster/ClusterMigrationController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.validation.annotation.Validated;
@@ -11,8 +12,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import jakarta.validation.Valid;
 
 import com.njydsz.common.audit.annotation.Audit;
 import com.njydsz.common.audit.enums.AuditAction;
