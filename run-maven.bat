95bf0a3aab4362ed4a5390bce120fa4008b36bcf
Marvin Lee <limw1888@126.com>
2026-09-03 10:15:08 +0800

    chore: 清理 WriteStyleHandler_head.java 临时文件 + run-maven.bat 换行符标准化

--- run-maven.bat
+++ run-maven.bat
@@ -1,4 +1,4 @@
 @echo off
 cd /d D:\Code\open\ydsz-cloud
-"D:\Maven\njydsz-maven\bin\mvn.cmd" -DskipTests=true -s "D:\Maven\njydsz-maven\conf\settings.xml" -Dmaven.repo.local="D:\Maven\njydsz-repo" clean install -f pom.xml > maven-build.log 2>&1
+"D:\Maven\njydsz-maven\bin\mvn.cmd" -DskipTests=true -s "D:\Maven\njydsz-maven\conf\settings.xml" -Dmaven.repo.local="D:\Maven\njydsz-repo" clean install -rf :ydsz-common-util -f pom.xml > maven-build.log 2>&1
 echo Exit code: %ERRORLEVEL%
