614971d1f9450f96a1afe26f966d58c491ec24f6
Marvin Lee <limw1888@126.com>
2026-09-05 17:00:49 +0800

    refactor(po): 优化实体类LocalDateTime导入顺序
    - 调整GenDatasourcePO中java.time.LocalDateTime导入位置
    - 调整GenHistoryPO中java.time.LocalDateTime导入位置
    - 调整GenTableMetaPO中java.time.LocalDateTime导入位置
    - 调整GenTemplateGroupPO中java.time.LocalDateTime导入位置
    - 调整GenTemplatePO中java.time.LocalDateTime导入位置

--- ydsz-generator/ydsz-generator-infra/src/main/java/com/njydsz/generator/po/GenHistoryPO.java
+++ ydsz-generator/ydsz-generator-infra/src/main/java/com/njydsz/generator/po/GenHistoryPO.java
@@ -1,11 +1,11 @@
 package com.njydsz.generator.po;
 
+import java.time.LocalDateTime;
+
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
-
-import java.time.LocalDateTime;
 
 /**
  * 代码生成任务历史持久化对象。
--- ydsz-generator/ydsz-generator-infra/src/main/java/com/njydsz/generator/po/GenTemplatePO.java
+++ ydsz-generator/ydsz-generator-infra/src/main/java/com/njydsz/generator/po/GenTemplatePO.java
@@ -1,11 +1,11 @@
 package com.njydsz.generator.po;
 
+import java.time.LocalDateTime;
+
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
-
-import java.time.LocalDateTime;
 
 /**
  * 模板持久化对象。
