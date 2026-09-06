8242677de7679bffe2063bd946721b97f4351876
Marvin Lee <limw1888@126.com>
2026-09-06 14:04:27 +0800

    refactor(userinfo): 移除 UserInfoNameAssembler 及其自动配置
    - 删除了 UserInfoNameAssembler 类及其所有相关方法实现
    - 移除 UserInfoNameAssemblerAutoConfiguration 自动配置类及相关注解和 Bean 注册
    - 新增 ydsz-gateway、ydsz-message、ydsz-system、ydsz-userinfo、ydsz-workflow、ydsz-literule、ydsz-cronjob 的 SkyWalking 链路追踪和日志 traceId 配置
    - 各服务通过 Java Agent 方式启用 SkyWalking，无需修改业务代码
    - 日志模式添加 traceId 和 spanId 支持，便于链路跟踪与定位问题

--- ydsz-userinfo/ydsz-userinfo-infra/src/main/java/com/njydsz/userinfo/infra/converter/UserInfoNameAssemblerAutoConfiguration.java
+++ ydsz-userinfo/ydsz-userinfo-infra/src/main/java/com/njydsz/userinfo/infra/converter/UserInfoNameAssemblerAutoConfiguration.java
@@ -1,4 +1,4 @@
-package com.njydsz.userinfo.domain.converter;
+package com.njydsz.userinfo.infra.converter;
 
 import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
